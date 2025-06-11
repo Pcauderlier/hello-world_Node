b15786b3df27dcbb009d38bbace69594cfb7b284
Pcauderlier <pierre.cauderlier@outlook.com>
2025-06-11 16:20:08 +0200

    ypp

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   });
 
 app.use((req,res,next) => {
-    res.status(200).json({message : "hello world"})
+    res.status(200).json({message : "hello worlds"})
 })
 
 const normalizePort = val => {
